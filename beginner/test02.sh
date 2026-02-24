056c470c56e6e47db7397316089504ed8887c55a
Mablenn <marcos.broseta@gmail.com>
2026-02-24 13:44:51 +0100

    Update beginner folder

---
+++ beginner/test02.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 1. Parámetros de un script
+# 2. Ejecutar comandos
+
+
+echo "================================="
+echo "Parámetros del script y ejecución"
+echo "de comandos."
+echo "================================="
+
