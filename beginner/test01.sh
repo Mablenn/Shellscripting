4e4e94ebff766e3c3e1c32f2cb02389ffd1f32fe
Mablen <marcos.broseta@gmail.com>
2026-02-24 13:30:01 +0100

    creado directorio beginner

---
+++ beginner/test01.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 1. Mostrar texto por consola
+# 2. Uso de variables
+
+# 1.
+# El comando 'echo' muestra líneas de texto por consola. Aunque, el comando 'printf' es mucho más completo,
+# de momento lo utilaremos para mostrar texto por pantalla.
+echo "=============================="
+echo "Empenzando con shellscripting."
+echo "=============================="
+echo "" # Genera un salto de línea
+
+# 2.
+# La declaración de variables siempre sigue el mismo patrón 'nombre=valor', sin espacios antes y después del signo '='
+# Diferencia entre definir variables numéricas o de texto:
+numero=240
+texto="siempre entre comillas dobles"
+
+# Mostrar variables
+echo "Valor de la variable 'numero': $numero"
+echo ""
+echo "Valor de la variable 'texto': $texto"
