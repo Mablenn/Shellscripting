1236fce15538f6201284371f416189f641dbbb63
Mablenn <marcos.broseta@gmail.com>
2026-02-25 14:02:48 +0100

    Creado test03.sh

---
+++ beginner/test03.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 1. Interacción con el usuario. Entrada de datos desde teclado
+# 2. Sustitución de comandos
+
+echo ""
+echo "================================="
+echo "Leer datos desde teclado"
+echo "================================="
+echo ""
+
+
+
+
